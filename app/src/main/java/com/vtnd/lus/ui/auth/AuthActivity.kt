c4e2993414d8c2baa0779219d9c73347ffc30de6
olololoe <olololoe1001st2@gmail.com>
2020-11-04 19:33:28 +0700

    Refs[Task][Auth][Login]build screen

---
+++ app/src/main/java/com/vtnd/lus/ui/auth/AuthActivity.kt
@@ -0,0 +1,20 @@
+package com.vtnd.lus.ui.auth
+
+import android.view.LayoutInflater
+import com.vtnd.lus.R
+import com.vtnd.lus.base.BaseActivity
+import com.vtnd.lus.databinding.ActivityAuthBinding
+import com.vtnd.lus.shared.extensions.addFragmentToActivity
+import com.vtnd.lus.ui.auth.login.LoginFragment
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class AuthActivity : BaseActivity<ActivityAuthBinding, AuthViewModel>() {
+    override val viewModel: AuthViewModel by viewModel()
+
+    override fun inflateViewBinding(inflater: LayoutInflater) =
+        ActivityAuthBinding.inflate(inflater)
+
+    override fun initialize() {
+        addFragmentToActivity(R.id.container, LoginFragment.newInstance(), false)
+    }
+}
